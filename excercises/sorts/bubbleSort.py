ebbbb1485c3f0ae3f6f4c73b364f61be481adba1
brucemolinanyc <brucemolinanyc@gmail.com>
2020-02-10 16:27:44 -0500

    updated insertion sort algo

--- excercises/sorts/bubbleSort.py
+++ excercises/sorts/bubbleSort.py
@@ -3,13 +3,11 @@ def bubbleSort(arr):
     counter = 0
     while not isSorted:
         isSorted = True 
-        for i in range(len(arr) - 1 - counter):
+        for i in range (len(arr) - 1 - counter):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 isSorted = False 
-        counter += 1 
-    return arr 
-
-
+        counter += 1
+    return arr
 
 print(bubbleSort([33,2,1,31,78,4,5,100,6,7]))
