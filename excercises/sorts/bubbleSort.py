b70c46215afde69f31326194fa07335bc925b747
brucemolinanyc <brucemolinanyc@gmail.com>
2020-02-12 10:48:35 -0500

    edited sorting algos

--- excercises/sorts/bubbleSort.py
+++ excercises/sorts/bubbleSort.py
@@ -1,13 +1,13 @@
 def bubbleSort(arr):
     isSorted = False 
-    counter = 0
+    counter = 0 
     while not isSorted:
         isSorted = True 
-        for i in range (len(arr) - 1 - counter):
+        for i in range(len(arr)-1-counter):
             if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                isSorted = False 
+                arr[i+1], arr[i] = arr[i], arr[i+1]
+                isSorted = False
         counter += 1
-    return arr
-
+    return arr 
+ 
 print(bubbleSort([33,2,1,31,78,4,5,100,6,7]))
