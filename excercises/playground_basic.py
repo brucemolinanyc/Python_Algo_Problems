5a6e18d4d2dec091f183e6592b9beba66ae600fe
brucemolinanyc <brucemolinanyc@gmail.com>
2020-03-18 11:55:38 -0400

    python practice

--- excercises/playground_basic.py
+++ excercises/playground_basic.py
@@ -58,4 +58,42 @@ def number_format():
 # number_format()
 
 #Write a Python program to accept a filename from the user and print the extension of that.
+def file_extension():
+    file = input('enter your filename: ')
+    while "." not in file:
+        print("please enter a proper filename with a '.' extension")
+        file = input("please enter a filename again: ")
+    file_type = file.split(".")[-1]
+    print(f'The extension of the file is {file_type}')
 
+# file_extension()
+
+#Write a Python program to display the first and last colors from the following list.
+def first_last(list):
+    print(f"the first item is {list[0]}")
+    print(f"the last item is {list[-1]}")    
+
+# first_last(['red', 'green', 'white', ' black'])
+
+#Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+def date(tuple):
+    if len(tuple) is not 3:
+        print("please enter a tuple format with three items")
+    else:
+        print(f'The examination will start from: {tuple[0]}/{tuple[1]}/{tuple[2]} ')
+
+# date((11,12,32,34))
+
+#Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. 
+def value_addition():
+    num = input("give me a number x and I will compute x + x + xxx: ")
+    while num.isdigit() is False:
+        print("please enter a number only")
+        num = input(" please enter a number x and I will compute x + x + xxx: ")
+    num1 = num 
+    num2 = num+num
+    num3 = num+num+num
+    result = int(num) + int(num2) + int(num3)
+    print(f"The result of {num1} + {num2} + {num3} is {result}")
+
+value_addition()
